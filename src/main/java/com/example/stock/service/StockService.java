67dcf5eee95af93aa8aa75bbbebd2b80493a93ff
NissyJeong <jnissi92@gmail.com>
2024-12-09 21:03:13 +0900

    동시성 문제 자바의 synchronized 로 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -14,8 +14,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+    //@Transactional // 우리가 만든 클래스를 매핑할 클래스를 새로 만들어서 실행하게 됨. 트랜잭션이 열리고 닫히기 전(업데이트 되기전) 다른 스레드가 해당 메서드를 호출하게 되면 동시성 이슈가 해결되지 않음
+    // synchronized 이용할때는 @Transactional 어노테이션을 사용하면 안됨.
+    public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고 감소
         // 갱신된 값을 저장
